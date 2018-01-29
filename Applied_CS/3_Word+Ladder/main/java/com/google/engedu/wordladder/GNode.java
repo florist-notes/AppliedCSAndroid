00284d22abda62a645a739f210545487b87395ea
Amartya R. Saikia <ar5saikia@gmail.com>
2018-01-29 18:08:38 +0530

    Update GNode.java

--- Applied_CS/3_Word+Ladder/main/java/com/google/engedu/wordladder/GNode.java
+++ Applied_CS/3_Word+Ladder/main/java/com/google/engedu/wordladder/GNode.java
@@ -2,7 +2,7 @@ package com.google.engedu.wordladder;
 
 // http://www.dreamincode.net/forums/topic/377473-graph-data-structure-tutorial/
 // This class is a Graph node
-// implementing this node in a different class helps in using it soothly
+// implementing this node in a different class helps in using it smoothly
 
 import java.util.ArrayList;
 
